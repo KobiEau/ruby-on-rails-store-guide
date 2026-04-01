e765c1ccd956688cc12bc8a15502c6d7e4a0588b
KobiEau <nana.enyan-aidoo@st.vvu.edu.gh>
2026-04-01 13:57:28 +0000

    feat: added controllers and before actions for some methods

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,55 @@
+class ProductsController < ApplicationController
+  allow_unauthenticated_access only: %i[index show]
+  before_action :set_product, only: %i[show edit update destroy]
+  #show all
+  def index
+    @products = Product.all
+  end
+ #show one product
+  def show
+  end
+ #create product
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      redirect_to @product
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @product.update(product_params)
+      redirect_to @product
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @product.destroy
+    redirect_to products_path
+  end
+
+  private
+  def set_product
+    @product = Product.find(params[:id])
+  end
+
+  def product_params
+  # params.require(:product).permit(:name, :price)
+
+  #.except(blacklist)
+  #.permit(whitelist)
+
+  #other code implementation
+  params.expect(product: [ :name, :price ])
+  end
+end
