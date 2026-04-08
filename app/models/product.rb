aff326b5c86f4195c8db3428d4766e04ce8bc711
KobiEau <nana.enyan-aidoo@st.vvu.edu.gh>
2026-04-08 17:35:45 +0000

    feat: created association between products, orders and order_items

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,7 @@
 class Product < ApplicationRecord
+  belongs_to :category,optional: true
+  has_many :order_items
+  has_many :orders, through: :order_items
   validates :name, presence: true, length: {minimum: 2, maximum: 100}
   validates :price, presence:true, numericality:{greater_than:0}
 end
