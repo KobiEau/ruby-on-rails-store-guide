be539bd0702357776a5ef58fa813d3e97e7fa1dd
KobiEau <nana.enyan-aidoo@st.vvu.edu.gh>
2026-04-08 17:37:16 +0000

    feat: created association between users and orders

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
-  has_secure_password
+  has_secure_password # handles password encryption
   has_many :sessions, dependent: :destroy
+  has_many :orders, dependent: :destroygit 
+  validates :email_address, presence: true, uniqueness:true
 
   normalizes :email_address, with: ->(e) { e.strip.downcase }
 end
