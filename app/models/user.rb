7a903d04d8fbad7a4ce526c5543f17948d9bc5a5
KobiEau <nana.enyan-aidoo@st.vvu.edu.gh>
2026-08-07 14:29:42 +0000

    fix: remove extraneous characters in orders association

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   has_secure_password # handles password encryption
   has_many :sessions, dependent: :destroy
-  has_many :orders, dependent: :destroygit 
+  has_many :orders, dependent: :destroy
   validates :email_address, presence: true, uniqueness:true
 
   normalizes :email_address, with: ->(e) { e.strip.downcase }
